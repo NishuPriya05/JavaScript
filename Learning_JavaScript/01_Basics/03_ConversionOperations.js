1f42ec63e7e6a38c5bd9ecb8bdd7eb8096f161a6
Nishu Priya <priyanishu0503@gmail.com>
2024-04-16 18:05:12 +0530

    config

---
+++ Learning_JavaScript/01_Basics/03_ConversionOperations.js
@@ -0,0 +1,13 @@
+let score = "33abd"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
