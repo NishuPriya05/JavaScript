54fca08ae3b73cc35301e993de6d7c2b340a9bf5
Nishu Priya <priyanishu0503@gmail.com>
2024-04-18 18:58:08 +0530

    Add existing files

--- Learning_JavaScript/01_Basics/03_ConversionOperations.js
+++ Learning_JavaScript/01_Basics/03_ConversionOperations.js
@@ -1,13 +1,27 @@
 let score = "33abd"
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+let value = 5
+let negValue = -value
+ 
+//console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Nikhil"
+
+str3 = str1 + str2
+
+console.log(str3)
---
+++ Learning_JavaScript/01_Basics/08_DateandTime.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+//  
+
+  let myCreatedDate = new Date("01-14-2024")
+
+
+  let myTimeStamp = Date.now()
+ 
+  console.log(myTimeStamp);
+
+  console.log(myCreatedDate.getTime())
+  console.log(Math.floor(Date.now()/1000))
+
+
+  let newDate = new Date()
+  console.log(newDate);
+  console.log(newDate.getMonth());
+  console.log(newDate.getDate());
