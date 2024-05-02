688a9d3649af9f5c616739779aebd3319f40f166
Nishu Priya <128479787+NishuPriya05@users.noreply.github.com>
2024-05-02 11:26:44 +0530

    Delete Learning_JavaScript directory

--- Learning_JavaScript/01_Basics/06_Strings.js
+++
@@ -1,6 +0,0 @@
- const name = "Nishu"
- const repoCount = "10"
-
- console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
- console.log(name.indexOf('h'))
- console.log(name.charAt(3))
