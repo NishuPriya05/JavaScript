78f5e8d8c92036a4d4d406d410c753447d5efe81
Nishu Priya <128479787+NishuPriya05@users.noreply.github.com>
2024-05-02 11:29:54 +0530

    Delete Learning_JavaScript directory

--- Learning_JavaScript/01_Basics/01_variables.js
+++
@@ -1,6 +0,0 @@
-const accountID = "54255"
-var accountEmail = "nishu@gmail.com"
-let accountPassword = "254825"
-
-console.log(accountEmail)
-console.table([accountID, accountEmail, accountPassword])
